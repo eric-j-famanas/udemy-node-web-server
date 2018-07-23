bcc52042ebe7d66a4cab1909ccc814ce12f65155
Eric Famanas <ejfamanas@hotmail.com>
2018-07-23 20:37:30 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to append log: ', error);
+        }
+    });
+    next();
+});
+
+app.use((request, response, next) => {
+    response.render('maintenance.hbs', {
+        pageTitle: 'Under Maintenance',
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Landing Page',
+        welcomeMessage: 'Hello World!',
+    })
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        statusCode: 400,
+        response: 'Bad Request',
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
